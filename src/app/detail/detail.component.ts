46739497431c54c0fb28482397f04951d79f16ae
LienaPuey <lienapuey@gmail.com>
2019-05-21 09:42:09 +0200

    variables

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -24,16 +24,16 @@ export class DetailComponent implements OnInit {
   ngOnDestroy() {
     this.sub.unsubscribe();
   }
-  timeConverter(date) {
-    var a = new Date(date * 1000);
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var year = a.getFullYear();
-    var month = months[a.getMonth()];
-    var date = a.getDate();
-    var hour = a.getHours();
-    var min = a.getMinutes();
-    var sec = a.getSeconds();
-    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec;
+  timeConverter(date: any) {
+    let a: any = new Date(date * 1000);
+    let months: any = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    let year: any = a.getFullYear();
+    let month: any = months[a.getMonth()];
+    let date: any = a.getDate();
+    let hour: any = a.getHours();
+    let min: any = a.getMinutes();
+    let sec: any = a.getSeconds();
+    let time: any = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
     return time;
   }
 }
